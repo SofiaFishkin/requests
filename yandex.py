bd8ade35ea5fd2a7cb4e1f2142066d6def126814
Rachel Fishkin <rachelfishkin@MacBook-Pro-Rachel.local>
2021-10-27 11:57:09 +0300

    first commit

--- yandex.py
+++ yandex.py
@@ -2,6 +2,7 @@ import os
 import requests
 import json
 
+
 class YDiskUploader:
 
     def __init__(self, file_path: str, token: str, destination_folder: str = "/"):
@@ -14,12 +15,12 @@ class YDiskUploader:
 
         self.destination_folder = "/"
         if len(destination_folder) != 0:
-             destination_folder = destination_folder.replace("\\", "/")
-             if not destination_folder.startswith("/"):
-                 destination_folder = "/" + destination_folder
-             if not destination_folder.endswith("/"):
-                 destination_folder += "/"
-             self.destination_folder = destination_folder
+            destination_folder = destination_folder.replace("\\", "/")
+            if not destination_folder.startswith("/"):
+                destination_folder = "/" + destination_folder
+            if not destination_folder.endswith("/"):
+                destination_folder += "/"
+            self.destination_folder = destination_folder
 
     def upload(self):
         result = {
@@ -27,41 +28,40 @@ class YDiskUploader:
             "msg": ""
         }
         if not os.path.isfile(self.file_path):
-             result["error"] = True
-             result["msg"] = f'Incorrect path to file: {self.file_path}'
-             return result
-
-        with open(self.file_path) as f:
-            filedata = f.read()
-
-        ydisk_path = self.destination_folder + os.path.basename(self.file_path)
-        params = {
-            "path": ydisk_path,
-            "overwrite": True
-        }
-
-        response = requests.get(self.url, params=params, headers=self.headers)
-        resp_info = json.loads(response.text)
-        ok_codes = (200, 201)
-        if not response.status_code in ok_codes:
-             resp_info = json.loads(response.text)
-             result["error"] = True
-             result["msg"] = f"Error with getting a link to file: {resp_info['message']}"
-             return result
-
-        response = requests.put(resp_info['href'], data = filedata)
-        if not response.status_code in ok_codes:
-            error_info = json.loads(response.text)
             result["error"] = True
-            result["msg"] = f"Error with attempt to upload the file to YDisk: {error_info['message']}"
+            result["msg"] = f'Incorrect path to file: {self.file_path}'
             return result
 
-        result["msg"] = "Upploaded successfully"
-        return result
+        with open(self.file_path, 'rb') as f:
+            filedata = f.read()
 
+            ydisk_path = self.destination_folder + os.path.basename(self.file_path)
+            params = {
+                "path": ydisk_path,
+                "overwrite": True
+            }
+
+            response = requests.get(self.url, params=params, headers=self.headers)
+            resp_info = json.loads(response.text)
+            ok_codes = (200, 201)
+            if not response.status_code in ok_codes:
+                resp_info = json.loads(response.text)
+                result["error"] = True
+                result["msg"] = f"Error with getting a link to file: {resp_info['message']}"
+                return result
+
+            response = requests.put(resp_info['href'], data=filedata)
+            if not response.status_code in ok_codes:
+                error_info = json.loads(response.text)
+                result["error"] = True
+                result["msg"] = f"Error with attempt to upload the file to YDisk: {error_info['message']}"
+                return result
+
+            result["msg"] = "Upploaded successfully"
+            return result
 if __name__ == '__main__':
     with open("token.txt", encoding="utf-8") as tokenfile:
         token = tokenfile.read().strip()
-    uploader = YDiskUploader('test.txt', token=token, destination_folder='/', encoding="utf-8")
+    uploader = YDiskUploader('test.txt', token=token, destination_folder='/')
     result = uploader.upload()
     print(result["msg"])
